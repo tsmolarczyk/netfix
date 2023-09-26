d9e26571b2f9d5fe56a9c0c50d7916dcfc20fcbb
Tomasz Smolarczyk <smolarczyk.thomas@gmail.com>
2023-09-26 20:35:23 +0200

    basic file structure added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,30 @@
+import { CssBaseline } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+
+import {
+  Actors,
+  MovieInformation,
+  Movies,
+  NavBar,
+  Profile
+} from "./components/index";
+
 function App() {
-  return <>App</>;
+  return (
+    <div style={{ display: "flex", height: "100%" }}>
+      <CssBaseline />
+      <NavBar />
+      <main style={{ flexGrow: "1", padding: "2em" }}>
+        <div style={{ height: "70px" }} />
+        <Routes>
+          <Route path="/movie/:id" element={<MovieInformation />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/" element={<Movies />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,6 @@
+const NavBar = () => {
+  console.log("NavBar");
+  return <div>NavBar</div>;
+};
+
+export default NavBar;
